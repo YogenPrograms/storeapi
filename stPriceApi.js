08f6254baa5ea048c8a7911b9992f625c7930f64
ygit*9881560218 <thakuryog1984@gmail.com>
2023-09-03 17:40:30 +0530

    fix

--- stPriceApi.js
+++ stPriceApi.js
@@ -86,7 +86,7 @@ res.send(result)
 });
 
 
-app.post("/",(req,res)=>{
+app.post("/",async(req,res)=>{
 
     const dataList = req.body
 //       const datalistCount= dataList.length
@@ -104,7 +104,7 @@ app.post("/",(req,res)=>{
       const url=dataList.result[i].url
   const title=dataList.result[i].title
   const thumbnail=dataList.result[i].thumbnail
-  let searchKey="yogen"
+  let searchKey=""+keyenter
   let company="AMAZON"
 
   const collectProduct={asin,url,title,thumbnail,searchKey,company}
@@ -115,7 +115,7 @@ app.post("/",(req,res)=>{
       const amazonProducts=Object.assign(collectProduct,price,reviews)
      console.log("datalist :",amazonProducts)
 
-    con.query('INSERT INTO productdb SET ?',amazonProducts,(error, result,fields) =>{
+   await con.query('INSERT INTO productdb SET ?',amazonProducts,(error, result,fields) =>{
         if(error)
         {
 return res.send(error)
@@ -124,5 +124,60 @@ return res.send(error)
 
 }
 return res.send("list of data")
+})
+
+app.post('/leftPro',(req,res)=>{
+
+    const mess=req.body
+    
+    const num=mess.data.length;
+
+for(let i=0;i<num;i++)
+{
+const productRating=mess.data[i].product_rating
+const productpageUrl=mess.data[i].product_page_url
+const productOffpageUrl=mess.data[i].product_page_url
+ const productSpecpageUrl=mess.data[i].product_specs_page_url
+const productReviewcpageUrl=mess.data[i].product_reviews_page_url
+const productNumReviewcpageUrl=mess.data[i].product_num_reviews
+ const productPhoto= mess.data[i].product_photos[0];
+ const product_title= mess.data[i].product_title;
+  const product_description= mess.data[i].product_description;
+
+
+  const store_name=mess.data[i].offer.store_name;
+//  const store_rating=mess.data[i].offer.store_rating;
+ const offer_page_url=mess.data[i].offer.offer_page_url;
+ const price=mess.data[i].offer.price;
+
+ searchKey=""+keyenter;
+
+
+const productD={productRating,productpageUrl,productOffpageUrl
+,productSpecpageUrl,productReviewcpageUrl,productNumReviewcpageUrl,productPhoto,product_title,
+product_description,store_name,offer_page_url,price,searchKey}
+
+// const productAttri=mess.data[i].product_attributes;
+
+
+
+// const productTile= mess.data.product_title;
+// const productDisc= mess.data.product_description;
+
+
+// const productDetails=Object.assign(productD,productAttri,off
+//     ,productTile,productDisc)
+
+console.log("products are:",productD)
+    
+     con.query('INSERT INTO leftproductdb SET ?',productD,(error, result,fields) =>{
+        if(error)
+        {
+return res.send(error)
+        }
+    })
+
+}
+return res.send("Left list of data")
 })
 app.listen(5000)
